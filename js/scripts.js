b0410cfae040cbf366a205d75eb0cc9bc403cc29
Faiza Husain and Jahmanz Williams <faiza@>
2018-01-10 13:20:47 -0800

    Added initial HTML and js file

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+
+
+    $("#shout").toggle();
+
+    event.preventDefault();
+  });
+});

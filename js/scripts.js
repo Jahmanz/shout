e5b9268e145569deaf3f216e9e07715f67936587
Faiza Husain and Jahmanz Williams <jahmanz@>
2018-01-10 13:43:57 -0800

    Added string method to make person2 input all CAPS.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
 
-
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
 
 
     $("#shout").toggle();
+
+    $("#yousaid").append("<li>" + person1Input + "</li>");
+    $("#webpagesaid").append(("<li>" + person2Input + "</li>").toUpperCase());
 
     event.preventDefault();
   });
